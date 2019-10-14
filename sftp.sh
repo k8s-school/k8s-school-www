50f70b8355d8621012d2e6ae31700a7a275112c6
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-10-14 18:22:21 +0200

    upd

--- sftp.sh
+++ sftp.sh
@@ -10,17 +10,23 @@ hugo
 # directory to backup
 SERVER_DIR="www"
 
-LOCAL_DIR="./public"
+LOCAL_DIR="$DIR/public"
+
+PDF_DIR="$DIR/pdf/
+
 
 . "$DIR/env-creds.sh"
 
 #yafc fish://"$SERVER_USER"@"$SERVER"
 
+htpasswd -bc "$PDF_DIR/.htaccess" "$HTACCESS_USER" "$HTACCESS_PASS" 
+cp -r "$PDF_DIR" "$LOCAL_DIR"
+
 yafc  <<**
 open fish://"$SERVER_USER":$SERVER_PASS@"$SERVER"
 mkdir "$SERVER_DIR"
 cd "$SERVER_DIR"
-rm -rf *
+# rm -rf *
 put -rf $LOCAL_DIR/*
 close
 **
